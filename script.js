ebc1eea764d1da13517edc4cc3f78c40b0cd0ecf
Steven Abaco <steve@wizardrywd.com>
2020-05-02 09:35:52 -0500

    Add build Script in package.json

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import { without } from 'lodash';
+var _ = require('lodash');
 
-console.log(without);
-
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log('answer:', _.without(array, 3));
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
